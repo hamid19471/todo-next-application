7e07609b70f17b9e7e41f92c6cf0f7418ab1b775
hamid19471 <hamid.19471@gmail.com>
2023-08-06 20:55:47 +0330

    Update: remove mongodb connection and data and use fake data and json server

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -1,8 +1,10 @@
-import dbConnect from "../../../server/utils/dbConnect";
-import Todo from "../../../server/models/todoModels";
-dbConnect();
+// import dbConnect from "../../../server/utils/dbConnect";
+// import Todo from "../../../server/models/todoModels";
+// dbConnect();
+
+import { todos } from "../../../data/todos";
 
 export default async function GET(req, res) {
-  const todos = await Todo.find({});
+  // const todos = await Todo.find({});
   return res.status(200).json({ todos });
 }

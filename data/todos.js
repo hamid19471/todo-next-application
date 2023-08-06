ec5db541712e80502776c9905a7e239f019af1f2
hamid19471 <hamid.19471@gmail.com>
2023-08-06 20:54:43 +0330

    Create: fake data of todos

---
+++ data/todos.js
@@ -0,0 +1,18 @@
+export const todos = [
+  {
+    id: 1,
+    title: "Reactjs",
+  },
+  {
+    id: 2,
+    title: "Nextjs",
+  },
+  {
+    id: 3,
+    title: "Nodejs",
+  },
+  {
+    id: 4,
+    title: "Vuejs",
+  },
+];

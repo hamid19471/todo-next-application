655f1e984cbd8a9211d12087a37933ad41bd7c9e
hamid19471 <hamid.19471@gmail.com>
2023-08-07 16:10:08 +0330

    Update: add description row for fake data

--- data/todos.js
+++ data/todos.js
@@ -2,17 +2,21 @@ export const todos = [
   {
     id: 1,
     title: "Reactjs",
+    descripti: "React js courses",
   },
   {
     id: 2,
     title: "Nextjs",
+    descripti: "next js courses",
   },
   {
     id: 3,
     title: "Nodejs",
+    descripti: "node js courses",
   },
   {
     id: 4,
     title: "Vuejs",
+    descripti: "vue js courses",
   },
 ];

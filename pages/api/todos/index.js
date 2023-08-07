1c0bcf294625c030571b624cfc0ebdd67520f686
hamid19471 <hamid.19471@gmail.com>
2023-08-07 16:11:24 +0330

    Update: change logic for post and get data from mongodb readl data base

--- pages/api/todos/index.js
+++ pages/api/todos/index.js
@@ -1,10 +1,18 @@
-// import dbConnect from "../../../server/utils/dbConnect";
-// import Todo from "../../../server/models/todoModels";
-// dbConnect();
+import dbConnect from "../../../server/utils/dbConnect";
+import Todo from "../../../server/models/todoModels";
 
-import { todos } from "../../../data/todos";
+dbConnect();
 
-export default async function GET(req, res) {
-  // const todos = await Todo.find({});
-  return res.status(200).json({ todos });
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    await Todo.create({
+      title: req.body.title,
+      description: req.body.description,
+    });
+    const todos = await Todo.find({});
+    return res.status(201).json({ message: "todo added", todos });
+  } else if (req.method === "GET") {
+    const todos = await Todo.find({});
+    return res.status(200).json({ todos });
+  }
 }
